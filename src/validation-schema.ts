26ffd988732b8f4a9c095f47f08251ce925e82c4
Christoph Heil <christoph.heil@etecture.de>
2019-11-28 14:28:02 +0100

    even more stuff

--- src/validation-schema.ts
+++ src/validation-schema.ts
@@ -1,7 +1,5 @@
 import * as Yup from 'yup'
 
-export const TextValidation = Yup.string().required()
-export const FileValidation = Yup.mixed().required()
-export const EmailValidation = Yup.string()
-  .required()
-  .email()
+export const TextValidation = Yup.string().required();
+export const FileValidation = Yup.mixed().required();
+export const EmailValidation = Yup.string().required().email();
